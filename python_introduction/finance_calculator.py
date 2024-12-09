a16ef8ab0d333e1c736bb25c84fefb586c2f1b10
rowlexy <rowlandelegbede@gmail.com>
2024-12-09 18:15:36 +0100

    added brackets

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -6,6 +6,6 @@ Monthly_Savings = income - expenses
 
 print(f"Your monthly savings are {Monthly_Savings}")
 
-Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
+Projected_Savings = (Monthly_Savings * 12) + (Monthly_Savings * 12 * 0.05)
 
 print(f"Projected savings after one year, with interest, is: {Projected_Savings}")
