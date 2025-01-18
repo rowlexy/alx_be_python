86f8b010db2e45277a07870bf297ccd630858296
Rowland <rowlandelegbede@gmail.com>
2025-01-18 15:04:16 +0100

    adjusted file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,18 +2,18 @@ class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
     def deposit(self, amount):
-        if amount <= 0:
+        if amount < 0:
             print("You can only deposit an amount greater than zero")
         else:
-            result = self.account_balance + amount
-            print(f"Deposited: ${result}")
+            self.account_balance += amount
+            print(f"Deposited: ${amount:.2f}")
     def withdraw(self, amount):
         if amount > self.account_balance:
             print("Insufficient funds.")
         elif amount <= 0:
             print("You can only withdraw an amount greater than zero")
         else:
-            result = self.account_balance - amount
-            print(f"Withdrew: ${result}")
+            self.account_balance -= amount
+            print(f"Withdrew: ${amount:.2f}")
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
