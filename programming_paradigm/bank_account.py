64b54f03f1108e5bb51bd9e6a48cb3a9fd321181
Rowland <rowlandelegbede@gmail.com>
2025-01-18 14:41:30 +0100

    refactored code

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,7 +16,7 @@ class BankAccount:
             result = self.account_balance - amount
             print(f"Withdrew: ${result}")
     def display_balance(self):
-        print(f'Current balance ${self.account_balance:.2f}')
+        print(f"Current Balance: ${self.account_balance:.2f}")
         
 account = BankAccount(100)
-account.display_balance()
+account.display_balance(["Current Balance:"])
