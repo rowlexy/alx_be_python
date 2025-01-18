af012bc3b0aad0ddb2caa5018f7ce16b1e4239ab
Rowland <rowlandelegbede@gmail.com>
2025-01-18 17:34:13 +0100

    added insertion

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
 def safe_divide(numerator, denominator):
-        numerator = int(input("Enter a number: "))
-        denominator = int(input("Enter a number: "))
+        numerator = float(input("Enter a number: "))
+        denominator = float(input("Enter a number: "))
         result = numerator / denominator
         print(f"The result of the division is {result:.1f}")
 try:
