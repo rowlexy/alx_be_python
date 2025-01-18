cb286b494c3dd2cc4928fd10400d29aa192c1176
Rowland <rowlandelegbede@gmail.com>
2025-01-18 18:17:04 +0100

    files added

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,10 +10,10 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 2), 3)
         self.assertEqual(self.calc.subtract(-2, -2), 0)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 5), 10)
         self.assertEqual(self.calc.multiply(0.5, 3), 1.5)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 2), 4.5)
 
