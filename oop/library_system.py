e23ca777ea5ac635ff80c1529b949aaa9a16609b
Rowland <rowlandelegbede@gmail.com>
2025-01-26 12:42:59 +0100

    Added oop files

---
+++ oop/library_system.py
@@ -0,0 +1,21 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBoook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, title, author,page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+class Library:
+    def add_book(self, book):
+        self.book = book
+
+    def list_books(self):
+        print(f"{self.book}")
